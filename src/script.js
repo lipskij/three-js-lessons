26198e1d690286758150a3f92888adf6d363bbb4
Emil Lipskij <emil.lipskij@gmail.com>
2021-08-24 11:55:50 +0300

    second commit

--- src/script.js
+++ src/script.js
@@ -19,10 +19,27 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Objects
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
+// const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+// const positionsAtribute = new THREE.BufferAttribute(positionsArray, 3);
+
+const geometry = new THREE.BufferGeometry();
+const count = 5000;
+const positionsArray = new Float32Array(count * 3 * 3);
+
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = Math.random();
+}
+
+const positionsAtribute = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionsAtribute);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+// new THREE.BoxGeometry(1, 1, 1, 2, 2, 2),
 scene.add(mesh);
 
 // Sizes
